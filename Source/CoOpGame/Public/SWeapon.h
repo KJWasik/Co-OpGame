ed09ca19da2b14cae27f856c23b4f451125737e9
LAPTOP-OJE1NJ4K\krzys <krzysztofwasik1988@wp.pl>
2019-05-05 18:39:47 +0100

    Working on applying damage. Muzzle and impact effects added.

--- Source/CoOpGame/Public/SWeapon.h
+++ Source/CoOpGame/Public/SWeapon.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Actor.h"
 #include "SWeapon.generated.h"
 
+class USkeletalMeshComponent;
+class UDamageType;
+class UParticleSystem;
+
 UCLASS()
 class COOPGAME_API ASWeapon : public AActor
 {
@@ -24,6 +28,18 @@ protected:
 
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
 	void Fire();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	TSubclassOf<UDamageType> DamageType;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	FName MuzzleSocketName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	UParticleSystem* MuzzleEffect;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	UParticleSystem* ImpactEffect;
 
 public:	
 	// Called every frame
