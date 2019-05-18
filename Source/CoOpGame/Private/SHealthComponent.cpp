a589fd32ccadca0adf524a7b70a0961121df2bbc
LAPTOP-OJE1NJ4K\krzys <krzysztofwasik1988@wp.pl>
2019-05-18 20:56:15 +0100

    Replicating health and death for multiplayer.

--- Source/CoOpGame/Private/SHealthComponent.cpp
+++ Source/CoOpGame/Private/SHealthComponent.cpp
@@ -3,11 +3,14 @@
 
 #include "SHealthComponent.h"
 #include "..\Public\SHealthComponent.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values for this component's properties
 USHealthComponent::USHealthComponent()
 {
 	DefaultHealth = 100;
+
+	SetIsReplicated(true);
 }
 
 
@@ -16,12 +19,16 @@ void USHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	AActor* MyOwner = GetOwner();
-	if (MyOwner)
+	// Only hook if we are server
+	if (GetOwnerRole() == ROLE_Authority)
 	{
-		MyOwner->OnTakeAnyDamage.AddDynamic(this, &USHealthComponent::HandleTakeAnyDamage);
+		AActor* MyOwner = GetOwner();
+		if (MyOwner)
+		{
+			MyOwner->OnTakeAnyDamage.AddDynamic(this, &USHealthComponent::HandleTakeAnyDamage);
+		}
 	}
-
+	
 	Health = DefaultHealth;
 }
 
@@ -37,4 +44,11 @@ void USHealthComponent::HandleTakeAnyDamage(AActor* DamagedActor, float Damage, 
 	UE_LOG(LogTemp, Log, TEXT("Health Changed: %s"), *FString::SanitizeFloat(Health));
 
 	OnHealthChanged.Broadcast(this, Health, Damage, DamageType, InstigatedBy, DamageCauser);
+}
+
+void USHealthComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(USHealthComponent, Health);
 }
